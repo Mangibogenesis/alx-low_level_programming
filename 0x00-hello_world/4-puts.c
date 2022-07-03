4824a5ad414af3f0f8475a73ad6f50486b58113b
Mangibogenesis <mangibogenesis81@gmail.com>
2022-07-03 14:37:08 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,29 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
 
-#include <string.h>                                                                                                                     
+#include <string.h>
 
-/**                                                                                                                                     
- *
- * *  * main - Entry point                                                                                                                
- *
- * *  * Return: Always 0 (Success)                                                                                                        
- *
- * */                                                                                                                                     
+/**
+ *  * main - Entry point
+ *   * Description: The programmer's life quote
+ *    * Return: Always 0 (success)
+ */
 
-int main(void)                                                                                                                          
+int main(void)
 
-{                                                                                                                                       
+{
 
-	puts("\"Programming is like building a multilingual puzzle");                                                                   
+		char str1[52];
 
-	return (0);                                                                                                                     
+
+
+			strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+
+
+				puts(str1);
+
+
+
+					return (0);
 
 }
